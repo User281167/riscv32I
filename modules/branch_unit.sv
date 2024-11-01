45bde3ecf9000911e469fa65bdea47d113c4170e
Juan <juandiego.velez@utp.edu.co>
2024-11-01 12:29:08 -0500

    Compare signed numbers, return 0 or 1 depending on branch operation

--- modules/branch_unit.sv
+++ modules/branch_unit.sv
@@ -5,17 +5,10 @@ module branch_unit(
     output logic branch_out
 );
     always @(*) begin
-        if (branch_op[4:3] == 2'b00) begin
-            branch_out = a == 0;
-        end
-        else if (branch_op == 5'b11111) begin
-            branch_out = 1;
-        end
-        else if (branch_op[4] == 1) begin
-            branch_out = a == 1;
-        end
-
         case (branch_op)
+            5'b00xxx: begin
+                branch_out = 0;
+            end
             5'b01000: begin
                 branch_out = a == b;
             end
@@ -23,16 +16,19 @@ module branch_unit(
                 branch_out = a != b;
             end
             5'b01100: begin
-                branch_out = a < b;
+                branch_out = $signed(a) < $signed(b); // by default input logic is unsigned
             end
             5'b01101: begin
-                branch_out = a >= b;
+                branch_out = $signed(a) >= $signed(b);
             end
             5'b01110: begin
                 branch_out = a < b;
             end
             5'b01111: begin
                 branch_out = a >= b;
+            end
+            5'b1xxxx: begin
+                branch_out = 1;
             end
         endcase
     end
--- tests/branch_unit.test.sv
+++ tests/branch_unit.test.sv
@@ -24,37 +24,42 @@ module test_branch_unit();
         #period;
 
         if (branch_out != 1) begin
-            $display("Test 1 failed");
+            $display("Test 1 (==) failed");
             $finish;
         end
 
         a = 32'h00000001;
         b = 32'h00000000;
-        branch_op = 5'b01000;
         #period;
 
         if (branch_out != 0) begin
-            $display("Test 2 failed");
+            $display("Test 2 (==) failed");
             $finish;
         end
 
-        a = 32'h00000001;
-        b = 32'h00000000;
-        branch_op = 5'b01001;
+        a = 32'hffffff9c;
+        b = 32'hffffff9c;
         #period;
 
         if (branch_out != 1) begin
-            $display("Test 3 failed");
+            $display("Test 3 (==) failed");
             $finish;
         end
 
-        a = 32'h00000001;
-        b = 32'h00000001;
         branch_op = 5'b01001;
         #period;
 
         if (branch_out != 0) begin
-            $display("Test 4 failed");
+            $display("Test 4 (!=) failed");
+            $finish;
+        end
+
+        a = 32'h00000001;
+        b = 32'h00000000;
+        #period;
+
+        if (branch_out != 1) begin
+            $display("Test 5 (!=) failed");
             $finish;
         end
 
@@ -64,27 +69,25 @@ module test_branch_unit();
         #period;
 
         if (branch_out != 0) begin
-            $display("Test 5 failed");
+            $display("Test 6 (<) failed");
             $finish;
         end
 
         a = 32'h00000000;
         b = 32'h00000001;
-        branch_op = 5'b01100;
         #period;
 
         if (branch_out != 1) begin
-            $display("Test 6 failed");
+            $display("Test 7 (<) failed");
             $finish;
         end
 
-        a = 32'h00000001;
-        b = 32'h00000001;
-        branch_op = 5'b01101;
+        a = 32'hffffff9c; // -100
+        b = 32'h00000032; // 50
         #period;
 
         if (branch_out != 1) begin
-            $display("Test 7 failed");
+            $display("Test 8 (<) failed");
             $finish;
         end
 
@@ -94,17 +97,87 @@ module test_branch_unit();
         #period;
 
         if (branch_out != 0) begin
-            $display("Test 8 failed");
+            $display("Test 9 (>=) failed");
             $finish;
         end
 
         a = 32'h00000002;
         b = 32'h00000001;
-        branch_op = 5'b01101;
         #period;
 
         if (branch_out != 1) begin
-            $display("Test 9 failed");
+            $display("Test 10 (>=) failed");
+            $finish;
+        end
+
+        a = 32'hffffff9c;
+        b = 32'h00000032;
+        #period;
+
+        if (branch_out != 0) begin
+            $display("Test 11 (>=) failed");
+            $finish;
+        end
+
+        a = 32'h00000032;
+        b = 32'hffffff9c;
+        #period;
+
+        if (branch_out != 1) begin
+            $display("Test 12 (>=) failed");
+            $finish;
+        end
+
+        a = 32'h00000032;
+        b = 32'hffffff9c;
+        branch_op = 5'b01110;
+        #period;
+
+        if (branch_out != 1) begin
+            $display("Test 13 (< U) failed");
+            $finish;
+        end
+
+        a = 32'h00000032;
+        b = 32'hffffff9c;
+        branch_op = 5'b01111;
+        #period;
+
+        if (branch_out != 0) begin
+            $display("Test 14 (>= U) failed");
+            $finish;
+        end
+
+        a = 32'hffffff9c;
+        b = 32'h00000032;
+        #period;
+
+        if (branch_out != 1) begin
+            $display("Test 15 (>= U) failed");
+            $finish;
+        end
+
+        branch_op = 5'b1xxxx;
+        #period;
+
+        if (branch_out != 1) begin
+            $display("Test 16 (1) failed");
+            $finish;
+        end
+
+        branch_op = 5'b10011;
+        #period;
+
+        if (branch_out != 1) begin
+            $display("Test 17 (1) failed");
+            $finish;
+        end
+
+        branch_op = 5'b00xxx;
+        #period;
+
+        if (branch_out != 0) begin
+            $display("Test 18 (0) failed");
             $finish;
         end
 
